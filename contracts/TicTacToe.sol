eabde7692ce8cfb1e1591087097915b307ce063e
Enzo Sagnelonge <enzo.sagnelonge@gmail.com>
2019-03-09 16:36:49 +0100

    d

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,4 +1,4 @@
-pragma  solidity ^0.5.4;
+pragma solidity ^0.5.0;
 
 /*
     This contract shows an example of the TicTacToe game written in Solidity.
@@ -14,6 +14,7 @@ contract TicTacToe {
         uint opponentBalance;
         address host;
         address opponent;
+        address winner;
         mapping(uint => mapping(uint => address)) board;  // (Row, Column) format
     }
 
@@ -42,6 +43,8 @@ contract TicTacToe {
     */
     modifier isHost(uint gameNumber, address player){
         //TODO De la même manière que isOpponent, valider que le joueur est bien le host de la partie
+        require(games[gameNumber].host == player, "Given player should be the host.");
+        _;
     }
 
     /*
@@ -70,6 +73,9 @@ contract TicTacToe {
     */
     modifier isCellFree(uint gameNumber, uint row, uint column){
         //TODO Valider que la case n'a jamais été jouée
+        if(games[gameNumber].board[row][column] == address(0)){
+            _;
+        }
     }
 
     modifier isValueProvided(uint value) {
@@ -85,29 +91,113 @@ contract TicTacToe {
     /*
         Returns the game numbers of the connected account
     */
-    function getHostGamesIds() {
+    function getHostGamesIds() public view returns (uint[] memory idsGames) {
         //TODO Retourner un array avec les numéro de jeux auquel le compte EOA appelant la fonction est inscrit.
+        idsGames = new uint[](getGamesNumber());
+        uint y = 0;
+        for (uint i=0; i<getGamesNumber(); i++) {
+            if(games[i].host == msg.sender || games[i].opponent == msg.sender){
+                idsGames[y] = i;
+                y++;
+            }
+        }
+
+        return idsGames;
     }
 
     /*
         The host can initiate the game for his address. He bet an initial amount of Ether.
     */
-    function initGame(address opponent) {
+    function initGame(address opponent, uint amountCash) public isValueProvided(amountCash){
         //TODO Créer un jeu et l'ajouter à la liste des jeux. Ce jeu doit enregistrer la mise du joueur le créant, ainsi que son addresse et l'addresse de l'adversaire qui sera passée en parametre.
+        Game memory game;
+        game.hostBalance = amountCash;
+        game.host = msg.sender;
+        game.opponent = opponent;
+        
+        games.push(game);
     }
 
     /*
         The opponent join the game.
     */
-    function joinGame(uint gameNumber) {
+    function joinGame(uint gameNumber, uint amountCash) public isOpponent(gameNumber, msg.sender) isValueProvided(amountCash){
         //TODO L'adversaire (un compte EOA) rejoint une partie qu'il choisit et sur laquelle il est déjà enregistré comme adversaire. Il doit mettre une mise initiale correspondant à celle mise par le Host.
+        games[gameNumber].opponentBalance = amountCash;
     }
 
     /*
         Play on a given cell
     */
-    function play(uint gameNumber, uint row, uint column) {
+    function play(uint gameNumber, uint row, uint column)  isPlayerTurn(gameNumber, msg.sender) isGameBalanceEqual(gameNumber) isCellFree(gameNumber, row, column) public{
         //TODO Un joueur place un pion sur un jeu auquel il s'est enregistré. Les mises des deux joueurs doivent être identiques pour jouer. Il faut également valider que la case n'a jamais été jouée.
+        games[gameNumber].board[row][column] = msg.sender;
+        games[gameNumber].turn += 1;
     }
 
+    function getHostId(uint gameNumber) public view returns (address hostId){
+        return  games[gameNumber].host;
+    }
+
+    function getOpponentId(uint gameNumber) public view returns (address opponentId){
+        return  games[gameNumber].opponent;
+    }
+
+    function getHostBalance(uint gameNumber) public view returns (uint hostBalance){
+        return  games[gameNumber].hostBalance;
+    }
+
+    function getOpponentBalance(uint gameNumber) public view returns (uint opponentBalance){
+        return  games[gameNumber].opponentBalance;
+    }
+
+    function getStateCell(uint gameNumber, uint row, uint column) public view returns (uint stateCell){
+        if(games[gameNumber].board[row][column] == games[gameNumber].host){
+            return 1;
+        }else if(games[gameNumber].board[row][column] == games[gameNumber].opponent){
+            return 2;
+        }else{
+            return 0;
+        }
+    }
+
+    function getWhoCanPlay(uint gameNumber) public view returns (uint player){
+        if((games[gameNumber].turn%2 == 0 && msg.sender == games[gameNumber].host) || (games[gameNumber].turn%2 == 1 && msg.sender == games[gameNumber].opponent)){
+            return 0;
+        }
+        else {
+            return 1;
+        }
+    }
+
+    function getWhoWinGame(uint gameNumber) public view returns (address winner){
+        
+        
+        if(games[gameNumber].board[0][0] == games[gameNumber].board[0][1]  && games[gameNumber].board[0][1] ==  games[gameNumber].board[0][2]){
+            winner =  games[gameNumber].board[0][0];
+        }else if(games[gameNumber].board[1][0] == games[gameNumber].board[1][1]  && games[gameNumber].board[1][2] == games[gameNumber].board[1][1]){
+            winner = games[gameNumber].board[1][0];
+        }else if(games[gameNumber].board[2][0] == games[gameNumber].board[2][1]  && games[gameNumber].board[2][2] == games[gameNumber].board[2][1]){
+            winner = games[gameNumber].board[2][0];
+        }else if(games[gameNumber].board[0][0] == games[gameNumber].board[1][0]  && games[gameNumber].board[2][0] == games[gameNumber].board[1][0]){
+            winner = games[gameNumber].board[0][0];
+        }else if(games[gameNumber].board[0][1] == games[gameNumber].board[1][1]  && games[gameNumber].board[2][1] == games[gameNumber].board[1][1]){
+            winner = games[gameNumber].board[0][1];
+        }else if(games[gameNumber].board[0][2] == games[gameNumber].board[1][2]  && games[gameNumber].board[2][2] == games[gameNumber].board[1][2]){
+            winner = games[gameNumber].board[0][2];
+        }else if(games[gameNumber].board[0][0] == games[gameNumber].board[1][1]  && games[gameNumber].board[2][2] == games[gameNumber].board[1][1]){
+            winner = games[gameNumber].board[1][1];
+        }else if(games[gameNumber].board[2][0] == games[gameNumber].board[1][1]  && games[gameNumber].board[0][2] == games[gameNumber].board[1][1]){   
+            winner = games[gameNumber].board[1][1];
+        }
+                
+                
+        if(games[gameNumber].board[0][0] == games[gameNumber].host || games[gameNumber].board[0][0] == games[gameNumber].opponent){
+            return winner;
+        }else{
+            return address(0);
+        }
+
+        
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -51,7 +51,7 @@ App = {
       App.contracts.TicTacToe.deployed().then(function (instance) {
 
         // Retrieve the game Ids and populate the board list
-        instance.getHostGamesIds().then(function(ids){
+        instance.getHostGamesIds().then(async function(ids){
           console.log(`ids : ${ids}`);
 
           var boardsDiv = document.getElementById("boards");
@@ -61,6 +61,32 @@ App = {
             var boardDiv = document.createElement("div");
             var h1 = document.createElement("h1");
             h1.innerHTML = `Board ${id}`;
+            
+            var host = document.createElement("p");
+
+            var hostId = await instance.getHostId(id);
+            var hostBalance = await instance.getHostBalance(id);
+            host.innerHTML = `Host player : ${hostId}  CashPrize : ${hostBalance}`;
+
+            var opponent = document.createElement("p");
+
+            var opponentId = await instance.getOpponentId(id);
+            var opponentBalance = await instance.getOpponentBalance(id);
+            opponent.innerHTML = `opponent player : ${opponentId}  CashPrize : ${opponentBalance}`;
+
+            var playerTurn = document.createElement("p");
+            var playerTurnId = await instance.getWhoCanPlay(id);
+
+            var winner = await instance.getWhoWinGame(id);
+            if(winner == hostId || winner == opponentId){
+              playerTurn.innerHTML = 'Winner Player : ' + winner;
+            }else if(playerTurnId == 0){
+              playerTurn.innerHTML = 'It s your turn';
+            }else{              
+              playerTurn.innerHTML = 'It s turn of your opponent';
+            }
+
+            
             var table = document.createElement("table");
 
             for(i=0 ; i<3 ; i++){
@@ -68,14 +94,33 @@ App = {
               tr.setAttribute("id", `row-${i}`);
               for(j=0 ; j<3 ; j++){
                 var td = document.createElement("td");
+                var btn = document.createElement("button");
+                btn.innerHTML = "Play"
+                if(playerTurnId == 0){
+                  btn.setAttribute('onClick', "App.handlePlayGame("+ id + "," + i + "," + j +")");
+                }
                 td.setAttribute("id", `col-${j}`);
-                td.innerHTML = "."; 
+                var stateCell = await instance.getStateCell(id,i,j);
+                //console.log("-- " + i + " " + j + "  " + stateCell)
+                if(stateCell == 0){
+                  td.appendChild(btn); 
+                }else if(stateCell == 1){
+                  td.innerHTML = "o"; 
+                }else if (stateCell == 2){
+                  td.innerHTML = "x";                   
+                }else{
+                  td.innerHTML = "bug"; 
+                }
+
                 tr.appendChild(td);
               }
               table.appendChild(tr);
             }
 
             boardDiv.appendChild(h1);
+            boardDiv.appendChild(host);
+            boardDiv.appendChild(opponent);
+            boardDiv.appendChild(playerTurn);
             boardDiv.appendChild(table);
             boardsDiv.appendChild(boardDiv);
           }
@@ -91,13 +136,27 @@ App = {
     var initGameValue = parseInt(document.getElementById("initGameValue").value);
     var opponentAddress = document.getElementById("initGameOpponent").value
 
-    App.contracts.TicTacToe.deployed().then(function (instance) {
-      instance.initGame(opponentAddress, { value: initGameValue }).then(function () { });
+    App.contracts.TicTacToe.deployed().then(
+      (instance) => {
+      instance.initGame(opponentAddress, initGameValue ).then(function () { });
     });
   },
 
   handleJoinGame: function () {
     //TODO L'opponent doit pouvoir faire joinGame en prenant les valeurs définies dans le html.
+    var joinGameValue = document.getElementById("joinGameValue").value;
+    var joinGameNumber = document.getElementById("joinGameNumber").value;
+    App.contracts.TicTacToe.deployed().then(
+      (instance) => {
+      instance.joinGame(joinGameNumber, joinGameValue ).then(function () { });
+    });
+  },
+
+  handlePlayGame: function (idGame, row, col){
+    App.contracts.TicTacToe.deployed().then(
+      (instance) => {
+      instance.play(idGame, row, col ).then(function () { });
+    });
   }
 
 };
